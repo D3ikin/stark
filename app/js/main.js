56f0d3b6bffd6126c2feaf0e6039fe7152c08eb0
D3ikin <deikincoin@gmail.com>
2021-09-12 15:55:35 +0300

    закончил десктоп, начал адаптацию

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,33 @@
 $(function(){
+  $('.faq__item-trigger').on('click', function(){
+    $(this).next('.faq__item-content').slideToggle(300);
+  });
+
+
+  $(".btn").click(function(){
+    $(".popup").fadeIn();
+    $("body").addClass('popup-active');
+  });
+  $(".popup_close").click(function(){
+    $(".popup").fadeOut();
+    $("body").removeClass('popup-active');
+  });
+  $(".popup").click(function(e){
+    if(e.target === this){
+      $(".popup").fadeOut();
+    }
+  });
   
+
+  // $(".send_mail_form").submit(function() {
+  //   var th = $(this);
+  //   $.ajax({
+  //     type: "POST",
+  //     url: "mail.php",
+  //     data: th.serialize()
+  //   }).done(function() {
+  //     $(".popup").fadeOut();
+  //   });
+  //   return false;
+  // });
 });
