d34982a04c3d8aad6227c9ed543547b3361b3b92
d3ikin <deikincoin@gmail.com>
2021-09-14 13:01:29 +0300

    конец, нужна оптимизация

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,22 @@
 $(function(){
+
+  $('.btn-burger').on('click', function(){
+    $('.btn-burger').toggleClass('btn-burger--active');
+    $('.header-bottom').toggleClass('header-bottom--active');
+  });
+
+  $(".header-bottom__list-link").click( function () {
+      $('.btn-burger').removeClass('btn-burger--active');
+      $('.header-bottom').removeClass('header-bottom--active');
+  });
+
   $(".header-bottom__list-link").on("click", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 300);
   });
+  
   $('.faq__item-trigger').on('click', function(){
     $(this).next('.faq__item-content').slideToggle(300);
   });
@@ -24,25 +36,15 @@ $(function(){
     }
   });
 
-  $('.btn-burger').on('click', function(){
-    $('.btn-burger').toggleClass('btn-burger--active')
-    $('.header-bottom').toggleClass('header-bottom--active')
+  $(".send_mail_form").submit(function() {
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php",
+      data: th.serialize()
+    }).done(function() {
+      $(".popup").fadeOut();
+    });
+    return false;
   });
-  $(".header-bottom__list-link").click( function () {
-      $('.btn-burger').removeClass('btn-burger--active')
-      $('.header-bottom').removeClass('header-bottom--active')
-  });
-  
-
-  // $(".send_mail_form").submit(function() {
-  //   var th = $(this);
-  //   $.ajax({
-  //     type: "POST",
-  //     url: "mail.php",
-  //     data: th.serialize()
-  //   }).done(function() {
-  //     $(".popup").fadeOut();
-  //   });
-  //   return false;
-  // });
 });
